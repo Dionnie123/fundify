391a3c5163c4c2dc1cdc3001137df1f2f99bf485
Mark Dionnie Bulingit <dionnie_bulingit@yahoo.com>
2024-04-14 16:38:22 +0800

    up

--- admin/class-fundify-admin.php
+++ admin/class-fundify-admin.php
@@ -142,38 +142,11 @@ class Fundify_Admin
 			return;
 		}
 
-		//Get the active tab from the $_GET param
-		$default_tab = null;
-		$tab = isset($_GET['tab']) ? $_GET['tab'] : $default_tab;
+
+		require_once 'partials/' . $this->plugin_name . '-admin-tabs.php';
 
 ?>
-<!-- Our admin page content should all be inside .wrap -->
-<div class="wrap">
-    <!-- Print the page title -->
-    <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
-    <!-- Here are our tabs -->
-    <nav class="nav-tab-wrapper">
-        <a href="?page=fundify" class="nav-tab <?php if ($tab === null) : ?>nav-tab-active<?php endif; ?>">General</a>
-        <a href="?page=fundify&tab=settings"
-            class="nav-tab <?php if ($tab === 'settings') : ?>nav-tab-active<?php endif; ?>">Settings</a>
-        <a href="?page=fundify&tab=tools"
-            class="nav-tab <?php if ($tab === 'tools') : ?>nav-tab-active<?php endif; ?>">Tools</a>
-    </nav>
 
-    <div class="tab-content">
-        <?php switch ($tab):
-					case 'settings':
-						echo 'Settings'; //Put your HTML here
-						break;
-					case 'tools':
-						echo 'Tools';
-						break;
-					default:
-						echo 'Default tab';
-						break;
-				endswitch; ?>
-    </div>
-</div>
 <?php
 	}
 
---
+++ admin/partials/fundify-admin-tabs.php
@@ -0,0 +1,33 @@
+<!-- Our admin page content should all be inside .wrap -->
+<?php
+//Get the active tab from the $_GET param
+$default_tab = null;
+$tab = isset($_GET['tab']) ? $_GET['tab'] : $default_tab;
+
+
+?>
+
+<div class="wrap">
+    <!-- Print the page title -->
+    <h1> Fundify </h1>
+    <!-- Here are our tabs -->
+    <nav class="nav-tab-wrapper">
+        <a href="?page=fundify" class="nav-tab <?php if ($tab === null) : ?>nav-tab-active<?php endif; ?>">General</a>
+        <a href="?page=fundify&tab=settings" class="nav-tab <?php if ($tab === 'settings') : ?>nav-tab-active<?php endif; ?>">Settings</a>
+        <a href="?page=fundify&tab=tools" class="nav-tab <?php if ($tab === 'tools') : ?>nav-tab-active<?php endif; ?>">Tools</a>
+    </nav>
+
+    <div class="tab-content wrap">
+        <?php switch ($tab):
+            case 'settings':
+                echo 'Settings'; //Put your HTML here
+                break;
+            case 'tools':
+                echo 'Tools';
+                break;
+            default:
+                echo 'Default tab';
+                break;
+        endswitch; ?>
+    </div>
+</div>
