d2de8f280e47c2b4afb1722177be46c617b281c0
Mark Dionnie Bulingit <dionnie_bulingit@yahoo.com>
2024-04-16 06:13:34 +0800

    up

--- admin/partials/fundify-admin-tabs.php
+++ admin/partials/fundify-admin-tabs.php
@@ -18,15 +18,47 @@ $tab = isset($_GET['tab']) ? $_GET['tab'] : $default_tab;
     <h1> Fundify </h1>
     <!-- Here are our tabs -->
     <nav class="nav-tab-wrapper">
-        <a href="?page=fundify-settings" class="nav-tab <?php if ($tab === null) : ?>nav-tab-active<?php endif; ?>">General</a>
-        <a href="?page=fundify-settings&tab=settings" class="nav-tab <?php if ($tab === 'settings') : ?>nav-tab-active<?php endif; ?>">Settings</a>
-        <a href="?page=fundify-settings&tab=tools" class="nav-tab <?php if ($tab === 'tools') : ?>nav-tab-active<?php endif; ?>">Tools</a>
+        <a href="?page=fundify-settings"
+            class="nav-tab <?php if ($tab === null) : ?>nav-tab-active<?php endif; ?>">General</a>
+        <a href="?page=fundify-settings&tab=settings"
+            class="nav-tab <?php if ($tab === 'settings') : ?>nav-tab-active<?php endif; ?>">Settings</a>
+        <a href="?page=fundify-settings&tab=tools"
+            class="nav-tab <?php if ($tab === 'tools') : ?>nav-tab-active<?php endif; ?>">Tools</a>
     </nav>
 
     <div class="tab-content wrap">
         <?php switch ($tab):
             case 'settings':
-                echo 'Settings'; //Put your HTML here
+               ?>
+        <div class="wrap">
+            <h1>Payment Integration Settings</h1>
+            <form method="post" action="options.php">
+                <?php
+            // Add nonce for security
+            settings_fields('payment_integration_group');
+            // Get the current option values
+            $options = get_option('payment_integration_options');
+            ?>
+
+                <table class="form-table">
+                    <tr valign="top">
+                        <th scope="row">API Key</th>
+                        <td><input type="text" name="payment_integration_options[api_key]"
+                                value="<?php echo esc_attr($options['api_key']); ?>" /></td>
+                    </tr>
+                    <tr valign="top">
+                        <th scope="row">Secret Key</th>
+                        <td><input type="text" name="payment_integration_options[secret_key]"
+                                value="<?php echo esc_attr($options['secret_key']); ?>" /></td>
+                    </tr>
+                </table>
+                <?php
+            // Add a submit button
+            submit_button('Save Settings');
+            ?>
+            </form>
+        </div>
+        <?php 
                 break;
             case 'tools':
                 echo 'Tools';
